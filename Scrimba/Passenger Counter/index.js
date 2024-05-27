f21b68d28e82c4f2451ce5646ffc46c7bf804ed5
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-05-26 22:51:03 -0400

    JavaScript Passenger Tracker

---
+++ Scrimba/Passenger Counter/index.js
@@ -0,0 +1,22 @@
+/*
+let count =5
+
+let countwithAdd=6+6
+console.log(count)
+console.log(countwithAdd) //This works
+
+let myAge=20
+let converter=7
+let convertedAge=myAge*converter
+
+console.log(convertedAge)
+*/
+let count=0
+//let countEl=document.getElementById("count-el")
+
+function increment(){
+console.log("The code is good")
+count+=1;
+document.getElementById("count-el").innerText=count
+}
+
