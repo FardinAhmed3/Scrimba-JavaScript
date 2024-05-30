577f8c0f3fe2af1c53228a590e615e14f63bd6c8
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-05-30 00:43:56 -0400

    Styling & Scripting

--- Blackjack Game/index.js
+++ Blackjack Game/index.js
@@ -2,14 +2,27 @@ let firstCard=5
 let secondCard=16
 
 let sum=firstCard+secondCard
+let hasBlackJack=false
+let isAlive=true
+let message=""
+
 
 if(sum <21){
-    console.log("Do you want to draw a new card?")
+    message=("Do you want to draw a new card?")
 } else if(sum === 21){
-    console.log("Blackjack!")
-} else if(sum > 21){
-    console.log("You're out!")
+    message=("Blackjack!")
+    hasBlackJack=true
+} else {
+    message=("You're out!")
+    isAlive=false
 }
 let age=21
 
-element=document.getElementById("")
+if(age<21){
+    console.log("You can not enter the game!")
+}else{
+    console.log("Welcome")
+}
+
+console.log(hasBlackJack)
+console.log(isAlive)
