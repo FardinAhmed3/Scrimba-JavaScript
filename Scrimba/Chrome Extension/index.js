e20f7f340de0acd68ca939da919559943611772d
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-06-02 04:49:10 -0400

    Chrome Extension Project

---
+++ Scrimba/Chrome Extension/index.js
@@ -0,0 +1,9 @@
+function saveLead(){
+    console.log("Button Clicked")
+}
+
+let inputBtn=document.getElementById("input-btn")
+inputBtn.addEventListener("click", function(){
+    console.log("Clicked through event listener")
+})
+
