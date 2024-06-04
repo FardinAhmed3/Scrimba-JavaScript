84e581f4e0b114a529abae30925a6d39b5817300
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-06-03 22:00:40 -0400

    Added functions to extension

--- Scrimba/Chrome Extension/index.js
+++ Scrimba/Chrome Extension/index.js
@@ -1,9 +1,21 @@
-function saveLead(){
-    console.log("Button Clicked")
-}
+//Initialization step
+myLeads=[]
+const inputEl=document.getElementById("input-el")
+const inputBtn=document.getElementById("input-btn")
+const uListEl=document.getElementById("ulist-el")
 
-let inputBtn=document.getElementById("input-btn")
+//Listening for button clicks
 inputBtn.addEventListener("click", function(){
-    console.log("Clicked through event listener")
-})
+    myLeads.push(inputEl.value)     //Pushing the element in the array
+    console.log(myLeads)            //Debug
+    renderLeads()                   //Call render function to display
+    inputEl.value=""                //Clear input field
+    })
 
+function renderLeads(){
+    let listItems=""
+for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li><a href='#'>" + myLeads[i] + "</a></li>"
+}
+uListEl.innerHTML =listItems
+}
